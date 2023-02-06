7116bf768bb39076a40d9e5f84ae0fbf01460b43
Shobhit Sharma <shobhit0812@gmail.com>
2023-02-06 22:06:20 +0530

    Update lib.groovy

--- vars/lib.groovy
+++ vars/lib.groovy
@@ -1,11 +1,9 @@
 def build()  {
     node {
-        // sh "git url: '${giturlrepo}'
-        git url: "git@github.com:webdevprashant/javaapp"
+        git url: "https://github.com/webdevprashant/javaapp
         sh "mvn clean package"
         sh "sudo docker build -t webdevprashant/javaapp:${BUILD_NUMBER} ."
         sh "sudo docker run  -d -p 1222:8080 --name myjavaapp webdevprashant/javaapp:${BUILD_NUMBER}" 
-        
         // sh "echo 'Build'"
     }
 }
